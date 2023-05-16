65c1897c4c79b14b2150f7f5a5888c4728517b35
Akira Kamada <125327118+AkiraKamada@users.noreply.github.com>
2023-05-16 09:39:32 +0900

    Add files via upload

--- web_01/loading.js
+++ web_01/loading.js
@@ -1,7 +1,11 @@
-window.onload = function() {
-    const spinner = document.getElementById('loading');
-    spinner.classList.add('loaded');
-}
+const loading = document.querySelector('.loading');
+
+window.addEventListener('load', () => {
+    setTimeout(function() {
+        loading.classList.add('loaded');
+    }, 1500);
+});
+
 document.getElementById('story').addEventListener('mouseover', function() {
     const sp = document.getElementById('story');
     sp.classList.add('story1');
